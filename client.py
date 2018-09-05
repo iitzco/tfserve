ff9a541eda9394378a0c82ba86cdf6b522d4aca1
Ivan Itzcovich <i.itzcovich@gmail.com>
2018-09-05 13:44:04 -0300

    Annotate the client

--- client.py
+++ client.py
@@ -1,16 +1,27 @@
 from PIL import Image
-from labels import LABEL_MAP
+from labels import label_map
 import numpy as np
 import tempfile
 
 from tfserve import TFServeApp
 
 
+# 1. Model: trained mobilenet on ImageNet that can be downloaded from
+#           https://storage.googleapis.com/mobilenet_v2/checkpoints/mobilenet_v2_1.4_224.tgz
 MODEL_PATH = "./mobilenet_v2_1.4_224/mobilenet_v2_1.4_224_frozen.pb"
+
+
+# 2. Input tensor names:
 INPUT_TENSORS = ["import/input:0"]
+
+# 3. Output tensor names:
 OUTPUT_TENSORS = ["import/MobilenetV2/Predictions/Softmax:0"]
 
 
+# 4. encode function: Receives raw jpg image as request_data. Returns dict
+#                     mappint import/input:0 to numpy value.
+#                     Model expects 224x224 normalized RGB image.
+#                     That is, [224, 224, 3]-size numpy array.
 def encode(request_data):
     with tempfile.NamedTemporaryFile(mode="wb", suffix=".jpg") as f:
         f.write(request_data)
@@ -20,11 +31,16 @@ def encode(request_data):
     return {INPUT_TENSORS[0]: img}
 
 
+# 5. decode function: Receives `dict` mapping import/MobilenetV2/Predictions/Softmax:0 to
+#                     numpy value and builds dict with for json response.
 def decode(outputs):
     p = outputs[OUTPUT_TENSORS[0]]
+    # p will be a 1001 numpy array with all class probabilities.
     index = np.argmax(p)
-    return {"class": LABEL_MAP[index-1], "prob": float(p[index])}
+    # This `dict` will result in a JSON response (courtesy of apistar).
+    return {"class": label_map[index-1], "prob": float(p[index])}
 
 
+# Run the server
 app = TFServeApp(MODEL_PATH, INPUT_TENSORS, OUTPUT_TENSORS, encode, decode)
 app.run('127.0.0.1', 5000, debug=True)
